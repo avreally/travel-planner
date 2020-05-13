d68352ed76972c8ec96beaa3a1ac4c582ab79acc
Valeria-Shadrina <waleria.shadrina@gmail.com>
2020-05-13 18:11:40 +0200

    Changed mode to production, added wp dev and prod files

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,6 +3,7 @@ const webpack = require('webpack')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+    mode: 'production',
     entry: './src/client/index.js',
     module: {
         rules: [
