262db6a957bf8f21773690ebc52168bc6f6284d1
Valeria-Shadrina <waleria.shadrina@gmail.com>
2020-05-13 18:38:44 +0200

    Added output library target and client

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,6 +6,10 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 module.exports = {
     mode: 'production',
     entry: './src/client/index.js',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
     module: {
         rules: [
             {
