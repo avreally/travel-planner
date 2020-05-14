37f78bd98cdc9532250d0461f49ff8fe3961767d
Valeria-Shadrina <waleria.shadrina@gmail.com>
2020-05-14 15:19:08 +0200

    Added service workers

--- src/client/index.js
+++ src/client/index.js
@@ -7,3 +7,14 @@ import './styles/style.scss'
 // console.log(checkForName);
 
 alert('OK!')
+
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js').then(registration => {
+            console.log('SW registered: ', registration);
+        }).catch(registrationError => {
+            console.log('SW registration failed: ', registrationError);
+        });
+    });
+}
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
     mode: 'production',
@@ -23,6 +24,13 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
+            title: 'Progressive Web Application',
+        }),
+        new WorkboxPlugin.GenerateSW({
+            // these options encourage the ServiceWorkers to get in there fast
+            // and not allow any straggling "old" SWs to hang around
+            clientsClaim: true,
+            skipWaiting: true,
         }),
         new CleanWebpackPlugin({
             // Simulate the removal of files
