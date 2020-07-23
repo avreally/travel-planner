8d824b0fa5b0475f862903468d9dbc0008488403
Valeria-Shadrina <waleria.shadrina@gmail.com>
2020-07-23 16:12:20 +0200

    Deleted commentsOnly files, comments

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -10,8 +10,6 @@ let apiKeyWeatherbit = '4f64fed98275458e9ed0a797ec81774e';
 let baseURLPixabay = 'https://pixabay.com/api/?image_type=photo&category=buildings&q=';
 let apiKeyPixabay = '6315616-d5cb7351229c7679827eaf034';
 
-// red,orange,yellow,green,turquoise,blue,lilac,pink,brown,white
-
 // Object with all data
 let primaryData;
 
@@ -33,10 +31,8 @@ function performAction(e) {
         alert('Date is not specified');
         return;
     }
-    // console.log(`Departure date: ${departureDate}`);
     primaryData.departureDate = departureDate;
     const daysLeft = differenceInDays(new Date(departureDate), Date.now());
-    // console.log(`Days left: ${daysLeft}`);
     primaryData.daysLeft = daysLeft;
     // GeoNames API
     const destination = document.getElementById('destination').value;
@@ -47,8 +43,6 @@ function performAction(e) {
     primaryData.destination = destination;
     getData(baseURLGeoNames, destination)
         .then(async (allData) => {
-            // console.log(allData);
-            // console.log(allData.geonames[0].countryName);
             const countryName = allData.geonames[0].countryName;
             primaryData.countryName = countryName;
             const lat = allData.geonames[0].lat;
@@ -59,26 +53,19 @@ function performAction(e) {
                     baseURLWeatherbitCurrent,
                     `key=${apiKeyWeatherbit}&lat=${lat}&lon=${lng}`
                 ).then((weatherDataCurrent) => {
-                    // console.log('Current weather:', weatherDataCurrent);
                     const temperature = weatherDataCurrent.data[0].temp;
                     const weatherDescription = weatherDataCurrent.data[0].weather.description;
                     primaryData.temperature = temperature;
                     primaryData.weatherDescription = weatherDescription;
-                    // console.log('Temperature:', temperature);
-                    // console.log('Weather:', weatherDescription);
                 });
             } else {
                 await getData(
                     baseURLWeatherbitDaily,
                     `key=${apiKeyWeatherbit}&lat=${lat}&lon=${lng}`
                 ).then((weatherDataDaily) => {
-                    // console.log('Daily weather:', weatherDataDaily);
                     const temperatureMax = weatherDataDaily.data[0].max_temp;
                     const temperatureMin = weatherDataDaily.data[0].min_temp;
                     const weatherDescriptionDaily = weatherDataDaily.data[0].weather.description;
-                    // console.log('Temperature Max:', temperatureMax);
-                    // console.log('Temperature Min:', temperatureMin);
-                    // console.log('Weather:', weatherDescriptionDaily);
                     primaryData.temperatureMax = temperatureMax;
                     primaryData.temperatureMin = temperatureMin;
                     primaryData.weatherDescription = weatherDescriptionDaily;
@@ -90,26 +77,19 @@ function performAction(e) {
                 // Pull in an image for the country from Pixabay API when the entered location brings up no results.
                 if (pictureData.totalHits > 0) {
                     const locationPhoto = pictureData.hits[0].webformatURL;
-                    // console.log(locationPhoto);
                     primaryData.pictureURL = locationPhoto;
                 } else {
-                    // console.log('No picture of city found');
                     await getData(
                         baseURLPixabay,
                         `${countryName}&key=${apiKeyPixabay}`
                     ).then(async (pictureDataCountry) => {
-                        // console.log(pictureDataCountry);
                         // Pull in a travel image from Pixabay API when trere is no picture of country.
                         if (pictureDataCountry.totalHits > 0) {
                             const countryPhoto = pictureDataCountry.hits[0].webformatURL;
-                            // console.log(countryPhoto);
                             primaryData.pictureURL = countryPhoto;
                         } else {
-                            // console.log('No picture of country found');
                             await getData(baseURLPixabay, `travel&key=${apiKeyPixabay}`).then((pictureDataMock) => {
-                                // console.log(pictureDataMock);
                                 const mockPhoto = pictureDataMock.hits[0].webformatURL;
-                                // console.log(mockPhoto);
                                 primaryData.pictureURL = mockPhoto;
                             });
                         }
@@ -162,9 +142,7 @@ const updateUI = () => {
         temperature.classList.add('text-entry');
         temperature.innerText = 'Temperature, ℃: ' + primaryData.temperature;
         resultsText.appendChild(temperature);
-    }
-
-    if (primaryData.temperatureMax !== '') {
+    } else if (primaryData.temperatureMax !== '') {
         let temperatureMax = document.createElement('div');
         temperatureMax.classList.add('text-entry');
         temperatureMax.innerText = 'Temperature, max ℃: ' + primaryData.temperatureMax;
